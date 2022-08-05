f68c8f008dc3a7bf59d1d7385c49c5d211a91578
khaledr <info@khaledr.ir>
2022-08-05 23:56:43 +0430

    refactoring

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -22,6 +22,8 @@ export const saveChanges = async (
      * In here the changes must be saved on MySQL database. But I run out of time.
      * Instead this will update the array anyway!!
      * Important!!: I have to refresh my knowlodge on MySQL as recently focuced on frontend.
+     *
+     *
      */
 
     const body = req.body.products as Array<IProduct>;
